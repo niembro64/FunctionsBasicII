7a977e4dcf0098cf9ecdf06603c93fa0c2be356a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-08 15:50:28 -0500

    Done 4

--- functionsBasicII.py
+++ functionsBasicII.py
@@ -31,6 +31,19 @@ print(first_plus_length([1,2,3,4,5]))
 # 4 Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 # Example: values_greater_than_second([3]) should return False
+print()
+def values_greater_than_second(a):
+    b = []
+    if len(a) < 2:
+        return False
+    for i in range(0,len(a)):
+        if a[i] > a[1]:
+            b.append(a[i])
+    print(len(b))
+    return b
+print(values_greater_than_second([5,2,3,2,1,4]))
+print(values_greater_than_second([3]))
+
 
 
 
